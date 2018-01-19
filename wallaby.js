eba583f9abdb3bd718fa01d15d652aa7be1fa3d4
Artem Govorov <artem.govorov@gmail.com>
2018-01-19 18:30:57 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -9,8 +9,7 @@ module.exports = function () {
     env: {
       type: 'node',
       params: {
-        env: 'ESM_OPTIONS=cjs',
-        runner: `-r ${require('path').join(__dirname, 'node_modules/@std/esm')}`
+        env: 'ESM_OPTIONS=cjs', runner: `-r ${require.resolve("@std/esm")}`
       }
     }
   }
